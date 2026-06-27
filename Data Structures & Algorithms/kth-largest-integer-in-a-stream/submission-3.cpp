346610ebaf7c89b83f8f1beefee55a341575b9c4
Petar Tešić <80098883+PosteruOle@users.noreply.github.com>
2026-06-27 19:50:25 +0200

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.cpp
@@ -0,0 +1,31 @@
+class KthLargest {
+private:
+    int k;
+    priority_queue<int> heap;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        
+        for(int num: nums){
+            heap.push(num);
+        }    
+    }
+    
+    int add(int val) {
+        heap.push(val);
+        vector<int> help;
+        int top;
+
+        for(int i=1;i<=k;i++){
+            top=heap.top();
+            help.push_back(top);
+            heap.pop();
+        }
+
+        for(int num: help){
+            heap.push(num);
+        }
+
+        return top;
+    }
+};
